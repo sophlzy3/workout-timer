2c119e717df39f77633f37bddc048addde1da261
sophlzy3 <96968503+sophlzy3@users.noreply.github.com>
2025-06-30 16:54:41 -0400

    deploy scripts

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,20 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command, mode }) => {
+  const isProduction = command === 'build'
+  const isStaticExport = process.env.BRANCH === 'static-export' || mode === 'static-export'
+  
+  return {
+    base: isProduction ? '/workout-timer/' : '/',
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: isStaticExport ? {} : {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    define: {
+      __IS_STATIC_EXPORT__: JSON.stringify(isStaticExport),
+    }
+  }
 })
